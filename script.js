89f2330c77755e62b367c521f0a8cdfa8bd4d132
Miguel <miguel11.macj@gmail.com>
2024-07-10 23:48:00 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function openv_emergente() {
+  document.getElementById("myv_emergente").style.display = "block";
+}
+
+function redirectToInstitutional() {
+  // Redirigir a la página de registro mediante correo institucional
+  window.location.href = "registroinst.html";
+}
+
+function redirectToOther() {
+  // Redirigir a la página de registro con otro correo
+  window.location.href = "registroppp.html";
+}
