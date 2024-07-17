cfdea852732394d0f0c375883988214d65f27d86
Lucheguas <luis.sebastian.loyola.vera.3@gmail.com>
2024-07-17 14:16:15 -0500

    arreglo de error menor: base de datos no conecta

--- assets/controladores/bd.php
+++ assets/controladores/bd.php
@@ -1,9 +1,9 @@
 <?php
-    //este apartado es para conectarme a la base de datos de login_register_db --> usuarios
-    $conexion = mysqli_connect("localhost:33065","root","", "proyecto_integrador");//si no funciona cambiar el localhost.
-    if (!$conexion) {
-        die("Error de conexión: " . mysqli_connect_error());
-    }
+//este apartado es para conectarme a la base de datos de login_register_db --> usuarios
+$conexion = mysqli_connect("localhost:33065", "root", "", "proyecto_integrador"); //si no funciona cambiar el localhost.
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
    /*
     SOLO USAR ESTO SI ES QUE HAY ERRORES EN LA CONEXION A LA BD
     SI NO, NO HAY MOTIVOS PARA USAR ESTO
@@ -14,4 +14,3 @@
     else{
         echo 'No se ha podido conectar a la base de datos';
     }*/
-    mysqli_close($conexion);
