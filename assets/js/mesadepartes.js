ce22f11a9397027e666150cd35782cfd32ad6b91
Lucheguas <luis.sebastian.loyola.vera.3@gmail.com>
2024-07-30 20:59:11 -0500

    documentando funciones de la mesa de partes

--- assets/js/mesadepartes.js
+++ assets/js/mesadepartes.js
@@ -1,183 +1,214 @@
+//resetform(); esta cometado porque daba errores en su implementacion.
+//Comente tanto la función como sus llamadas
+
+//ESTA FUNCIÓN OCULTA Y MUESTRA EL SIDEBAR
 function toggleMenu() {
-    const sidebar = document.getElementById('sidebar');
-    sidebar.style.display = sidebar.style.display === 'none' ? 'block' : 'none';
-  }
+  const sidebar = document.getElementById('sidebar');
+  sidebar.style.display = sidebar.style.display === 'none' ? 'block' : 'none';
+}
 
-  //PARA CARGAR LOS MODALS DE LA PAGINA
-  function loadProfileForm() {
-    /*
-    resetForm(); este funcion era la que hacia que desapareciera todo*/
-    closeBienvenidaContainer();
-    const profileContainer = document.getElementById('profileContainer');
-    const solicitudContainer = document.getElementById('solicitudContainer');
-    profileContainer.style.display = 'block';
-    solicitudContainer.style.display = 'none';
-  }
-  
-  function loadSolicitudForm() {
-    /*resetForm();*/
-    closeBienvenidaContainer();
-    const profileContainer = document.getElementById('profileContainer');
-    const solicitudContainer = document.getElementById('solicitudContainer');
-    profileContainer.style.display = 'none';
-    solicitudContainer.style.display = 'block';
-  }
+//PARA CARGAR LOS APARTADOS DE LA PÁGINA
 
-  function loadRegistroSolicitud(){
-    closeSolicitudForm();
-    const registroContainer = document.getElementById('Next-step');
-    registroContainer.style.display = 'block';
-    closeBienvenidaContainer();
-  }
+function loadProfileForm() {
+  //esta función hace que se cargue el apartado de los datos del perfil
+  /*resetForm();*/
+  closeBienvenidaContainer();
+  const profileContainer = document.getElementById('profileContainer');
+  const solicitudContainer = document.getElementById('solicitudContainer');
+  profileContainer.style.display = 'block';
+  solicitudContainer.style.display = 'none';
+}
 
-  function loadSegundoForm(){
-    const segundoFormContainer = document.getElementById('nuevoContainer');
-    segundoFormContainer.style.display = 'block';
-  }
+function loadSolicitudForm() {
+  //esta funcion carga el apartado de la carta de presentación (1er tramite)
+  /*resetForm();*/
+  closeBienvenidaContainer();
+  const profileContainer = document.getElementById('profileContainer');
+  const solicitudContainer = document.getElementById('solicitudContainer');
+  profileContainer.style.display = 'none';
+  solicitudContainer.style.display = 'block';
+}
 
-  function loadBienvenidaContainer(){
-    const bienvenidaContainer = document.getElementById('bienvenidaContainer');
-    bienvenidaContainer.style.display = 'block';
-  }
+function loadRegistroSolicitud(){
+  //esta funcion carga el apartado del 1form(registro)
+  //de la carta de presentacion
+  closeSolicitudForm();
+  const registroContainer = document.getElementById('Next-step');
+  registroContainer.style.display = 'block';
+  closeBienvenidaContainer();
+}
 
-  function closeBienvenidaContainer(){
-    const bienvenidaContainer = document.getElementById('bienvenidaContainer');
-    bienvenidaContainer.style.display = 'none';
-  }
+function loadSegundoForm(){
+  //esta funcion carga el apartado del 2form (subida de futs)
+  //de la carta de presentacion
+  const segundoFormContainer = document.getElementById('nuevoContainer');
+  segundoFormContainer.style.display = 'block';
+}
 
-  function closeProfileForm() {
-    const profileContainer = document.getElementById('profileContainer');
-    profileContainer.style.display = 'none';
-    loadBienvenidaContainer();
-  }
-  
-  function closeSolicitudForm() {
-    const solicitudContainer = document.getElementById('solicitudContainer');
-    solicitudContainer.style.display = 'none';
-    loadBienvenidaContainer();
-  }
+function loadBienvenidaContainer(){
+  //esta funcion carga el apartado de bienvenida a la pagina web
+  const bienvenidaContainer = document.getElementById('bienvenidaContainer');
+  bienvenidaContainer.style.display = 'block';
+}
 
-  function closeRegistroSolicitud(){
-    
-    const registroSolicitud = document.getElementById('Next-step');
-    registroSolicitud.style.display = 'none';
-    loadSegundoForm();
-  }
+function closeBienvenidaContainer(){
+  //esta funcion cierra el apartado de bienvenida de la pag web
+  const bienvenidaContainer = document.getElementById('bienvenidaContainer');
+  bienvenidaContainer.style.display = 'none';
+}
 
-  function closeSegundoForm(){
-    const segundoFormContainer = document.getElementById('nuevoContainer');
-    segundoFormContainer.style.display = 'none';
-  }
-  /*
+function closeProfileForm() {
+  //esta funcion cierra el apartado de la muestra de datos del perfil
+  const profileContainer = document.getElementById('profileContainer');
+  profileContainer.style.display = 'none';
+  loadBienvenidaContainer();
+}
+
+function closeSolicitudForm() {
+  //esta funcion cierra el apartado de bienvenida de la carta
+  //de presentación
+  const solicitudContainer = document.getElementById('solicitudContainer');
+  solicitudContainer.style.display = 'none';
+  loadBienvenidaContainer();
+}
+
+function closeRegistroSolicitud(){
+  //este apartado cierra el 1form de la carta de presentación
+  const registroSolicitud = document.getElementById('Next-step');
+  registroSolicitud.style.display = 'none';
+  loadSegundoForm();
+}
+
+function closeSegundoForm(){
+  //este apartado cierra el 2form de la carta de presentación
+  const segundoFormContainer = document.getElementById('nuevoContainer');
+  segundoFormContainer.style.display = 'none';
+}
+
+function openModal() {
+  document.getElementById('imageModal').style.display = 'block';
+}
+
+function closeModal() {
+  document.getElementById('imageModal').style.display = 'none';
+}
+/*
+function uploadImage() {
+  alert('Cargar imagen');
+}
+
+function removeImage() {
+  alert('Quitar imagen');
+}
+
+function saveImage() {
+  alert('Imagen guardada');
+  closeModal();
+}*/
+
+function closeModal() {
+  document.getElementById('imageModal').style.display = 'none';
+}
+
+function openModal() {
+  document.getElementById('imageModal').style.display = 'block';
+}
+
+/*
   function Aformulario() {  
     document.getElementById('solicitudContainer').style.display = 'none';
     document.getElementById('nuevoContainer').style.display = 'block';
   }*/
-  //FIN DE LA SECCION ANTERIOR
 
-  function openModal() {
-    document.getElementById('imageModal').style.display = 'block';
+/*
+No usamos el updateProfilfe de js
+
+function updateProfile() {
+  const isNameValid = validateName();
+  const isCodigoValid = validateCodigo();
+  const isEmailValid = validateEmail();
+  const isPhoneValid = validatePhone();
+
+  const nameInput = document.getElementById('name');
+  if (nameInput.value.trim() === '') {
+      alert('Complete el campo "Apellidos y nombres".');
+      return;
   }
-  
-  function closeModal() {
-    document.getElementById('imageModal').style.display = 'none';
+
+  if (!isNameValid || !isCodigoValid || !isEmailValid || !isPhoneValid) {
+      alert('Por favor, corrige los errores en el formulario.');
+      return;
   }
-  
-  function uploadImage() {
-    alert('Cargar imagen');
+
+  alert('Perfil actualizado correctamente.');
+}*/
+
+/*
+//VALIDACIONES
+function validateName() {
+  const nameInput = document.getElementById('name');
+  const nameError = document.getElementById('name-error');
+  const regex = /^[a-zA-Z\s]*$/;
+  if (!regex.test(nameInput.value)) {
+      nameError.textContent = 'El campo "Apellidos y nombres" solo puede contener letras y espacios.';
+      return false;
+  } else {
+      nameError.textContent = '';
+      return true;
   }
-  
-  function removeImage() {
-    alert('Quitar imagen');
+}
+
+function validateCodigo() {
+  const codigoInput = document.getElementById('codigo');
+  const codigoError = document.getElementById('codigo-error');
+  const regex = /^\d{10}$/;
+  const codigoValue = codigoInput.value;
+
+  if (!regex.test(codigoValue)) {
+      codigoError.textContent = 'El código debe ser un número de 10 cifras.';
+      return false;
   }
-  
-  function saveImage() {
-    alert('Imagen guardada');
-    closeModal();
+
+  const codigoNumber = parseInt(codigoValue, 10);
+  if (codigoNumber < 2018000000 || codigoNumber > 2024000000) {
+      codigoError.textContent = 'El código debe estar entre 2018000000 y 2024000000.';
+      return false;
   }
-  //VALIDACIONES
-  function validateName() {
-    const nameInput = document.getElementById('name');
-    const nameError = document.getElementById('name-error');
-    const regex = /^[a-zA-Z\s]*$/;
-    if (!regex.test(nameInput.value)) {
-        nameError.textContent = 'El campo "Apellidos y nombres" solo puede contener letras y espacios.';
-        return false;
-    } else {
-        nameError.textContent = '';
-        return true;
-    }
+
+  codigoError.textContent = '';
+  return true;
+}
+
+function validateEmail() {
+  const emailInput = document.getElementById('email');
+  const emailError = document.getElementById('email-error');
+  const regex = /^[a-zA-Z0-9._%+-]+@(unfv\.edu\.pe|hotmail\.com|gmail\.com)$/;
+  if (!regex.test(emailInput.value)) {
+      emailError.textContent = 'Ingrese un correo electrónico válido';
+      return false;
+  } else {
+      emailError.textContent = '';
+      return true;
   }
+}
   
-  function validateCodigo() {
-    const codigoInput = document.getElementById('codigo');
-    const codigoError = document.getElementById('codigo-error');
-    const regex = /^\d{10}$/;
-    const codigoValue = codigoInput.value;
-  
-    if (!regex.test(codigoValue)) {
-        codigoError.textContent = 'El código debe ser un número de 10 cifras.';
-        return false;
-    }
-  
-    const codigoNumber = parseInt(codigoValue, 10);
-    if (codigoNumber < 2018000000 || codigoNumber > 2024000000) {
-        codigoError.textContent = 'El código debe estar entre 2018000000 y 2024000000.';
-        return false;
-    }
-  
-    codigoError.textContent = '';
-    return true;
+function validatePhone() {
+  const phoneInput = document.getElementById('phone');
+  const phoneError = document.getElementById('phone-error');
+  const regex = /^\d{9}$/;
+  if (!regex.test(phoneInput.value)) {
+      phoneError.textContent = 'El número de celular debe ser un número de 9 cifras.';
+      return false;
+  } else {
+      phoneError.textContent = '';
+      return true;
   }
-  
-  function validateEmail() {
-    const emailInput = document.getElementById('email');
-    const emailError = document.getElementById('email-error');
-    const regex = /^[a-zA-Z0-9._%+-]+@(unfv\.edu\.pe|hotmail\.com|gmail\.com)$/;
-    if (!regex.test(emailInput.value)) {
-        emailError.textContent = 'Ingrese un correo electrónico válido';
-        return false;
-    } else {
-        emailError.textContent = '';
-        return true;
-    }
-  }
-  
-  function validatePhone() {
-    const phoneInput = document.getElementById('phone');
-    const phoneError = document.getElementById('phone-error');
-    const regex = /^\d{9}$/;
-    if (!regex.test(phoneInput.value)) {
-        phoneError.textContent = 'El número de celular debe ser un número de 9 cifras.';
-        return false;
-    } else {
-        phoneError.textContent = '';
-        return true;
-    }
-  }
-  
-  function updateProfile() {
-    const isNameValid = validateName();
-    const isCodigoValid = validateCodigo();
-    const isEmailValid = validateEmail();
-    const isPhoneValid = validatePhone();
-  
-    const nameInput = document.getElementById('name');
-    if (nameInput.value.trim() === '') {
-        alert('Complete el campo "Apellidos y nombres".');
-        return;
-    }
-  
-    if (!isNameValid || !isCodigoValid || !isEmailValid || !isPhoneValid) {
-        alert('Por favor, corrige los errores en el formulario.');
-        return;
-    }
-  
-    alert('Perfil actualizado correctamente.');
-  }
+}
+
 //////////////////////////////////////////////
-  /*
-  No la estamos usando
+
+  No estamos usando esta seccion
+
   function resetForm() {
     const nameInput = document.getElementById('name');
     const codigoInput = document.getElementById('codigo');
@@ -197,17 +228,13 @@ function toggleMenu() {
     codigoError.textContent = '';
     emailError.textContent = '';
     phoneError.textContent = '';
-  }*/
+  }
   
-  
-  let currentImage = null; 
+let currentImage = null; 
 
-/*
 function openFileInput() {
     document.getElementById('fileInput').click();
-}*/
-
-/*
+}
 function handleFileSelect(event) {
   const file = event.target.files[0];
   const imageButton = document.getElementById('image-button');
@@ -233,24 +260,18 @@ function handleFileSelect(event) {
       document.getElementById('imagePreview').style.display = 'none';
       currentImage = null;
   }
-}*/
-
-
-
+}
 
 document.getElementById('fileInput').addEventListener('change', handleFileSelect);
-/*
 function uploadImage() {
     document.getElementById('fileInput').click();
-}*/
-/*
+}
 function removeImage() {
     const previewImage = document.getElementById('previewImage');
     previewImage.src = '';
     document.getElementById('imagePreview').style.display = 'none';
     currentImage = null;
-}*/
-/*
+}
 function saveImage() {
     if (currentImage) {
         alert('Imagen guardada');
@@ -259,8 +280,7 @@ function saveImage() {
     } else {
         alert('Cargue una imagen primero.');
     }
-}*/
-/*
+}
 function updateMainImageButton() {
     if (currentImage) {
         const mainImageButton = document.getElementById('image-button');
@@ -272,10 +292,3 @@ function updateMainImageButton() {
     }
 }*/
 
-function closeModal() {
-    document.getElementById('imageModal').style.display = 'none';
-}
-
-function openModal() {
-    document.getElementById('imageModal').style.display = 'block';
-}
