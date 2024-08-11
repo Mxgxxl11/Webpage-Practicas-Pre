977a1ab5584906726a2a4a3942c60ae5a494d072
Lucheguas <luis.sebastian.loyola.vera.3@gmail.com>
2024-08-11 15:00:53 -0500

    arregla problema de arreglar administradores en la tabla docentes en registrar administradores y docentes

--- assets/controladores/registro_admin.php
+++ assets/controladores/registro_admin.php
@@ -21,6 +21,7 @@ $Escuela = $_POST['escuela'];
 //para sacar la fecha de creacion de la cuenta
 $fecha_actual = date("Y-m-d");
 //recordar que idRoles ahora esta en otra tabla
+//aca se guardan los admin
 $query = "INSERT INTO usuario(codigo, contraseña, correo, fecha_creacion, nombre1,
     nombre2, apellido1, apellido2, celular, direccion, id_escuela, id_tipodoc, numDocumento, distrito, nro_departamento)
     VALUES ('$codigo','$password','$correo', '$fecha_actual', '$primerNombre','$segundoNombre',
@@ -29,6 +30,7 @@ $query = "INSERT INTO usuario(codigo, contraseña, correo, fecha_creacion, nombr
 //guardando codigo y idRol en la tabla tipo_usuario
 $query2 = "INSERT INTO acceso(id_usuario, id_rol) VALUES ('$codigo', '$rol')";
 $query3 = "INSERT INTO docente(id_usuario) VALUES ('$codigo')";
+$query4 = "INSERT INTO paso_cp(paso, id_usuario) VALUES (0, '$codigo')";
 
 $verificarCorreo = mysqli_query($conexion, "SELECT * FROM usuario WHERE correo = '$correo' "); //le estoy pidiendo que me verifique los correos que sean iguales
 if (mysqli_num_rows($verificarCorreo) > 0) { //
@@ -50,14 +52,26 @@ if (mysqli_num_rows($verificarCodigo) > 0) { //
         ';
     exit(); //para salir del script actual y que no se ejecute el codigo de abajo.
 }
+$verificarDOC = mysqli_query($conexion, "SELECT * FROM usuario WHERE  numDocumento= '$numDoc' "); //le estoy pidiendo que me verifique los numDOC que sean iguales
+if (mysqli_num_rows($verificarDOC) > 0) { //
+    echo '
+            <script>
+                alert("Numero de documento ya registrado, ingrese un correo válido.");
+                window.location = "./../../agregar_usuario.php";
+            </script>
+        ';
+    exit(); //para salir del script actual y que no se ejecute el codigo de abajo.
+}
 
 //Para cuando le demos a registrar
 $ejecutar = mysqli_query($conexion, $query); // RECORDANDO: primero ingresamos la base, y luego ingresamos a los campos de la base
 $ejecutar2 = mysqli_query($conexion, $query2);
-$ejecutar3 = mysqli_query($conexion, $query3);
+
+$ejecutar4 = mysqli_query($conexion, $query4);
 
 if ($ejecutar and $ejecutar2) {
     if ($rol == 2) {
+        $ejecutar3 = mysqli_query($conexion, $query3);
         if ($ejecutar3) {
             echo '
             <script>
