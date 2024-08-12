05be5bab623185586a05f2d14ddcfc6e984ca51e
Miguel <miguel11.macj@gmail.com>
2024-08-12 01:58:46 -0500

    Update tramites.php

--- tramites.php
+++ tramites.php
@@ -82,15 +82,15 @@ $mostrarDiv = isset($_SESSION['paso_cp']) ? $_SESSION['paso_cp'] : '';
 
             <div class="responsive">
                 <div class="gallery">
-                    <img src="https://cdn-icons-png.flaticon.com/512/4420/4420106.png" alt="constancia">
+                    <img src="https://cdn-icons-png.flaticon.com/512/4420/4420106.png" style="height: 32%;" alt="evaluación">
                     <div class="desc">
                         <div class="ambos">
-                            <p class="texto_izquierdo" id="text3"><strong>CONSTANCIA</strong></p>
+                            <p class="texto_izquierdo" id="text3"><strong>EVALUACION FINAL</strong></p>
                         </div>
                         <p class="texto_derecho" id="text2"><strong>Incompleto</strong></p>
                         <p class="texto_izquierdo" id="text1"><strong>Documentos:</strong></p>
-                        <p class="texto_izquierdo" id="text4">- Constancia de Practicas Pre Profesionales</p>
-                        <button onclick="abrir_constancia()">Iniciar</button>
+                        <p class="texto_izquierdo" id="text4">- Examen final resuelto</p>
+                        <button onclick="abrir_evaluacion()">Iniciar</button>
                     </div>
                 </div>
             </div>
