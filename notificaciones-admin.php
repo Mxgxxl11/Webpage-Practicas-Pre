0ecf8dac44b5091213e643771302d62fcd8fc2f0
Miguel <miguel11.macj@gmail.com>
2024-08-13 04:19:24 -0500

    Update notificaciones-admin.php

--- notificaciones-admin.php
+++ notificaciones-admin.php
@@ -1,209 +1,148 @@
-<!-- Esta página muestra TODOS los usuarios registrados en el sistema
- IMPORTANTE: Pertenece al apartado de ADMINISTRADORES-->
-<?php
-session_start();
-include './assets/controladores/bd.php';
-if (empty($_SESSION['codigo_institucional'])) {
-    echo '<script>
-    alert("Para continuar debe iniciar sesión");
-    window.location = "login.html"; 
-    </script>';
-}
+<?php  
+session_start();  
+include './assets/controladores/bd.php';  
+if (empty($_SESSION['codigo_institucional'])) {  
+    echo '<script>  
+    alert("Para continuar debe iniciar sesión");  
+    window.location = "login.html";   
+    </script>';  
+}  
+$codigo_admin = $_SESSION['codigo_institucional'];
+?>  
+<!DOCTYPE html>  
+<html lang="en">  
 
-?>
-<!DOCTYPE html>
-<html lang="en">
+<head>  
+    <meta charset="UTF-8">  
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">  
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+    <title>Administradores</title>  
+    <link rel="stylesheet" href="assets/css/mesadepartes.css" />  
+    <link rel="stylesheet" href="assets/css/detalles.css">  
+    <style>  
+        .my-form__button {  
+            padding: 10px 20px;  
+            border: none;  
+            border-radius: 4px;  
+            background-color: darkorange;  
+            color: #fff;  
+            font-size: 16px;  
+            cursor: pointer;  
+            transition: background-color 0.3s;  
+        }  
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Administradores</title>
-    <link rel="stylesheet" href="assets/css/mesadepartes.css" />
-    <link rel="stylesheet" href="assets/css/detalles.css">
-    <style>
-        .my-form__button {
-            padding: 10px 20px;
-            border: none;
-            border-radius: 4px;
-            background-color: darkorange;
-            color: #fff;
-            font-size: 16px;
-            cursor: pointer;
-            transition: background-color 0.3s;
-        }
+        .my-form__button:hover {  
+            background-color: chocolate;  
+        }  
+    </style>  
+</head>  
 
-        .my-form__button:hover {
-            background-color: chocolate;
-        }
-    </style>
-</head>
+<body>  
+<header>  
+    <?php include './includes/header.php'; ?>  
+</header>  
+<div class="container">  
+    <?php include './includes/sidebar-admin.php'; ?>  
+    <article class="table-widget">  
+        <div class="caption">  
+            <h2>TODAS LAS NOTIFICACIONES</h2>  
+        </div>  
+        <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">  
+            <table>  
+                <tr>  
+                    <td>  
+                        <label for="codigo">Codigo</label>  
+                        <input type="number" id="codigo" name="codigo" autocomplete="off">  
+                    </td>  
+                    <td>  
+                        <label for="rol">Rol</label>  
+                        <input type="text" id="rol" name="rol" autocomplete="off">  
+                    </td>  
+                    <td>  
+                        <input class="my-form__button" type="submit" name="enviar" value="Buscar">  
+                    </td>  
+                    <td>  
+                        <div class="tags">  
+                            <div class="tag tag--marketing">  
+                                <a href="ver_usuarios.php" style="text-decoration:none; color:black;">  
+                                    Mostrar todas las notificaciones  
+                                </a>  
+                            </div>  
+                        </div>  
+                    </td>  
+                </tr>  
+            </table>  
+        </form>  
+        <br>  
+        <table>  
+            <thead>  
+                <tr>  
+                    <th>Nombre Alumno</th>  
+                    <th>Apellido Alumno</th>  
+                    <th>Mensaje</th>  
+                    <th>Fecha-Hora</th>
+                    <th>Leido</th>    
+                </tr>  
+            </thead>  
+            <tbody id="team-member-rows">  
+                <?php  
+                if (isset($_POST['enviar'])) {  
+                    // Busqueda en notificaciones  
+                    $codigo = $_POST['codigo'];  
+                    $rol = $_POST['rol'];  
+                    if (empty($codigo) && empty($rol)) {  
+                        echo '<script>  
+                        alert("Ingresa algún dato para buscar");  
+                        window.location = "./ver_usuarios.php";   
+                        </script>';  
+                    } else {  
+                        $busqueda = "  
+                        SELECT u.nombre1, u.apellido1, n.mensaje, n.fecha_notificacion, n.leido  
+                        FROM notificaciones n   
+                        JOIN usuario u ON u.codigo = n.id_usuario  
+                        WHERE n.id_usuario <> '$codigo_admin'";  
 
-<body>
-    <header>
-        <?php include './includes/header.php'; ?>
-    </header>
-    <div class="container">
-        <?php include './includes/sidebar-admin.php' ?>
-        <article class="table-widget">
-            <div class="caption">
-                <h2>
-                    TODOS LOS USUARIOS REGISTRADOS
-                </h2>
-            </div>
-            <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
-                <table>
-                    <tr>
-                        <td>
-                            <label for="codigo">Codigo</label>
-                            <input type="number" id="codigo" name="codigo" autocomplete="off">
-                        </td>
-                        <td>
-                            <label for="rol">Rol</label>
-                            <input type="text" id="rol" name="rol" autocomplete="off">
-                        </td>
-                        <td>
-                            <input class="my-form__button" type="submit" name="enviar" value="Buscar">
-                        </td>
-                        <td>
-                            <div class="tags">
-                                <div class="tag tag--marketing">
-                                    <a href="ver_usuarios.php" style="text-decoration:none; color:black;">
-                                        Mostrar todos los usuarios
-                                    </a>
-                                </div>
-                            </div>
-                        </td>
-                    </tr>
-                </table>
-            </form>
-            <br>
-            <table>
-                <thead>
-                    <tr>
-                        <th>
-                            Descripcion
-                        </th>
-                        <th>
-                            Estado
-                        </th>
-                        <th>
-                            Fecha
-                        </th>
-                        <th>
-                            Subido por
-                        </th>
-                        <th>
-                            Rol
-                        </th>
-                    </tr>
-                </thead>
-                <tbody id="team-member-rows">
-                    <?php
-                    if (isset($_POST['enviar'])) {
-                        //aca es para la busqueda
-                        $codigo = $_POST['codigo'];
-                        $rol = $_POST['rol'];
-                        if (empty($codigo) and empty($rol)) { // si no funciona codigo cambiar a $_POST['codigo']
-                            echo '<script>
-                            alert("Ingresa algún dato para buscar");
-                            window.location = "./ver_usuarios.php"; 
-                            </script>';
-                        } else {
-                            if (empty($rol)) {
-                                $busqueda = "SELECT a.id_usuario,r.nombre_rol,u.codigo,u.nombre1,u.apellido1, u.correo,u.fecha_creacion  
-                                    FROM usuario u
-                                    JOIN acceso a ON u.codigo = a.id_usuario
-                                    JOIN roles r ON a.id_rol = r.id_rol
-                                    WHERE u.codigo like '%" . $codigo . "%'";
-                            }
-                            if (empty($codigo)) {
-                                $busqueda = "SELECT a.id_usuario,r.nombre_rol,u.codigo,u.nombre1,u.apellido1, u.correo,u.fecha_creacion  
-                                FROM usuario u
-                                JOIN acceso a ON u.codigo = a.id_usuario
-                                JOIN roles r ON a.id_rol = r.id_rol
-                                WHERE r.nombre_rol like '%" . $rol . "%'";
-                            }
-                            if (!empty($rol) and !empty($codigo)) {
-                                $busqueda = "SELECT a.id_usuario,r.nombre_rol,u.codigo,u.nombre1,u.apellido1, u.correo,u.fecha_creacion  
-                                FROM usuario u
-                                JOIN acceso a ON u.codigo = a.id_usuario
-                                JOIN roles r ON a.id_rol = r.id_rol
-                                WHERE u.codigo like '%" . $codigo . "%' and r.nombre_rol like '%" . $rol . "%'";
-                            }
-                        }
-                        $ejec = mysqli_query($conexion, $busqueda);
-                        if ($ejec) {
-                            while ($filas = mysqli_fetch_assoc($ejec)) { ?>
-                                <tr>
-                                    <td>
-                                        <div class="profile-info">
-                                            <?php echo $filas['codigo'] ?>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div class="profile-info">
-                                            <?php echo $filas['nombre1'] . ' ' . $filas['apellido1']; ?>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div class="profile-info">
-                                            <?php echo $filas['nombre_rol'] ?>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <?php echo $filas['correo']; ?>
-                                    </td>
-                                    <td>
-                                        <div class="profile-info" style="text-align:center;">
-                                            <?php echo $filas['fecha_creacion']; ?>
-                                        </div>
-                                    </td>
-                                </tr>
-                            <?php }
-                        }
-                    } else {
-                        //aca para mostrar todos los registros
-                        //esta consulta solo llamará a los admins (cambiar el a.id_rol a lo que quieras llamar)
-                        $consulta = "SELECT a.id_usuario,r.nombre_rol,u.codigo,u.nombre1,u.apellido1, u.correo,u.fecha_creacion  
-                                    FROM usuario u
-                                    JOIN acceso a ON u.codigo = a.id_usuario
-                                    JOIN roles r ON a.id_rol = r.id_rol";
-                        $ejecucion = mysqli_query($conexion, $consulta);
-                        while ($filas = mysqli_fetch_assoc($ejecucion)) { ?>
-                            <tr>
-                                <td>
-                                    <div class="profile-info">
-                                        <?php echo $filas['codigo'] ?>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div class="profile-info">
-                                        <?php echo $filas['nombre1'] . ' ' . $filas['apellido1']; ?>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div class="profile-info">
-                                        <?php echo $filas['nombre_rol'] ?>
-                                    </div>
-                                </td>
-                                <td>
-                                    <?php echo $filas['correo']; ?>
-                                </td>
-                                <td>
-                                    <div class="profile-info" style="text-align:center;">
-                                        <?php echo $filas['fecha_creacion']; ?>
-                                    </div>
-                                </td>
-                            </tr>
-                    <?php }
-                    } ?>
-                </tbody>
-            </table>
-            <?php mysqli_close($conexion) ?>
-        </article>
-    </div>
-    <script src="assets/js/mesadepartes.js"></script>
-</body>
+                        // Añadir lógica de búsqueda adicional si es necesario aquí...  
+                    }  
+                    $ejec = mysqli_query($conexion, $busqueda);  
+                    if ($ejec && mysqli_num_rows($ejec) > 0) {  
+                        while ($filas = mysqli_fetch_assoc($ejec)) { ?>  
+                            <tr>  
+                                <td><?php echo $filas['nombre1']; ?></td>  
+                                <td><?php echo $filas['apellido1']; ?></td>  
+                                <td><?php echo $filas['mensaje']; ?></td>  
+                                <td><?php echo $filas['fecha_notificacion']; ?></td> 
+                                <td><?php echo $filas['leido']; ?></td> 
+                            </tr>  
+                        <?php }  
+                    } else {  
+                        echo '<tr><td colspan="4">No se encontraron notificaciones.</td></tr>';  
+                    }  
+                } else {  
+                    // Mostrar todas las notificaciones  
+                    $consulta = "  
+                    SELECT u.nombre1, u.apellido1, n.mensaje, n.fecha_notificacion, n.leido   
+                    FROM notificaciones n   
+                    JOIN usuario u ON u.codigo = n.id_usuario  
+                    WHERE n.id_usuario <> '$codigo_admin'";  
+
+                    $ejecucion = mysqli_query($conexion, $consulta);  
+                    while ($filas = mysqli_fetch_assoc($ejecucion)) { ?>  
+                        <tr>  
+                            <td><?php echo $filas['nombre1']; ?></td>  
+                            <td><?php echo $filas['apellido1']; ?></td>  
+                            <td><?php echo $filas['mensaje']; ?></td>  
+                            <td><?php echo $filas['fecha_notificacion']; ?></td>  
+                            <td><?php echo $filas['leido']; ?></td>  
+                        </tr>  
+                    <?php }  
+                } ?>  
+            </tbody>  
+        </table>  
+        <?php mysqli_close($conexion) ?>  
+    </article>  
+</div>  
+<script src="assets/js/mesadepartes.js"></script>  
+</body>  
 
 </html>
