aaeb0839080cc166db40563ebf7cdfd2c92b09ff
Miguel <miguel11.macj@gmail.com>
2024-08-13 02:52:00 -0500

    Add files via upload

---
+++ assets/controladores/subir_nt.php
@@ -0,0 +1,35 @@
+<?php  
+include 'bd.php'; 
+session_start();  
+$codigo = $_SESSION['codigo_institucional'];
+$nt = $_POST['nt'];
+
+$query2 = "UPDATE alumno SET nt = '$nt' WHERE id_usuario = '$codigo';";
+$stmt2 = mysqli_prepare($conexion, $query2);
+if (!$stmt2) {  
+    echo "Error en la preparación de la consulta: " . mysqli_error($conexion);  
+    exit();  
+} 
+$ejecutar2 = mysqli_stmt_execute($stmt2); 
+
+$query = "UPDATE paso_cp SET paso = 4 WHERE id_usuario = '$codigo'";
+$stmt = mysqli_prepare($conexion, $query);
+
+if (!$stmt) {  
+    echo "Error en la preparación de la consulta: " . mysqli_error($conexion);  
+    exit();  
+} 
+$ejecutar = mysqli_stmt_execute($stmt); 
+
+if ($ejecutar and $ejecutar2) {    
+    $_SESSION['paso_cp'] = '4';
+    echo ' Datos almacenados exitosamente ';  
+} else {  
+    echo '  
+            alert("Error al almacenar los datos. Inténtelo nuevamente.");   
+         ';  
+    echo "Error: " . mysqli_error($conexion);  
+}
+
+mysqli_close($conexion);
+?>
