4e199d5cc5d59fd6a2599e2b1f3909a212a418c6
Miguel <miguel11.macj@gmail.com>
2024-08-14 03:32:10 -0500

    Update subir_nota_exam_final.php

--- assets/controladores/subir_nota_exam_final.php
+++ assets/controladores/subir_nota_exam_final.php
@@ -1,48 +1,26 @@
-<?php  
-include 'bd.php'; 
-session_start();  
-$codigo = $_SESSION['codigo_institucional'];
-$codigo_a = $_POST['codigo_a'];
-$calificacion_reporte = $_POST['calificacion_reporte'];
-
-$result = mysqli_query($conexion, "SELECT id_alumno FROM alumno WHERE id_usuario = '$codigo_a'");
-$row = mysqli_fetch_assoc($result);
-$id_alumno = $row['id_alumno'];
-
 $result2 = mysqli_query($conexion, "SELECT id_docente FROM docente WHERE id_usuario = '$codigo'");
 $row2 = mysqli_fetch_assoc($result2);
 $id_docente = $row2['id_docente'];
 
-$result3 = mysqli_query($conexion, "SELECT id_carpeta FROM carpeta_virtual WHERE id_alumno = $id_alumno");
-$row3 = mysqli_fetch_assoc($result3);
-$id_carpeta = $row3['id_carpeta'];
+$query2 = "UPDATE calificaciones SET examen_final = '$nota_e' WHERE (id_docente = '$id_docente' AND id_alumno = '$id_alumno');";
+$stmt2 = mysqli_prepare($conexion, $query2);
+if (!$stmt2) {  
+    echo "Error en la preparación de la consulta: " . mysqli_error($conexion);  
+    exit();  
+} 
+$ejecutar2 = mysqli_stmt_execute($stmt2); 
 
-$result4 = mysqli_query($conexion, "SELECT id_archivo FROM archivos WHERE (nombre_archivo = 'Informe Final' AND id_carpeta = $id_carpeta)");
-$row4 = mysqli_fetch_assoc($result4);
-$id_archivo = $row4['id_archivo'];
+ $query = "UPDATE paso_cp SET paso = 14 WHERE id_usuario = '$codigo_a'";
+ $stmt = mysqli_prepare($conexion, $query);
 
-$sql = "INSERT INTO calificaciones (id_archivo, id_alumno, id_docente, trabajo_final) VALUES (?, ?, ?, ?)"; 
-$stmt = $conexion->prepare($sql);
-$stmt->bind_param("iiii", $id_archivo, $id_alumno, $id_docente, $calificacion_reporte);       
-if ($stmt->execute()) {  
-    echo "Archivo almacenado correctamente en la base de datos.";
-    //$_SESSION['paso_cp'] = '4';  
-} else {  
-    echo "Error al almacenar el archivo: " . $stmt->error;  
-}
-$stmt->close();
-
-$query = "UPDATE paso_cp SET paso = 11 WHERE id_usuario = '$codigo_a'";
-$stmt = mysqli_prepare($conexion, $query);
-
-if (!$stmt) {  
-   echo "Error en la preparación de la consulta: " . mysqli_error($conexion);  
+ if (!$stmt) {  
+    echo "Error en la preparación de la consulta: " . mysqli_error($conexion);  
     exit();  
 } 
  $ejecutar = mysqli_stmt_execute($stmt); 
 
- if ($ejecutar) {    
-    $_SESSION['paso_cp'] = '11';
+if ($ejecutar2) {    
+    $_SESSION['paso_cp'] = '14';
     echo ' Datos almacenados exitosamente ';  
 } else {  
     echo '  
