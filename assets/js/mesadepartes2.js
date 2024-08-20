005396ae42f8f2d074aad46b87f341ce65b2d924
Miguel <miguel11.macj@gmail.com>
2024-08-19 21:56:19 -0500

    Update mesadepartes2.js

--- assets/js/mesadepartes2.js
+++ assets/js/mesadepartes2.js
@@ -155,6 +155,9 @@ $(document).ready(function() {
 
   $(document).ready(function() {  
     $('#d_e_f').click(function() {  
+        // Obtener los valores de los inputs  
+        var id_alumno = $('#id_alumno').val();  
+        
         // Redirigir a la URL de descarga  
         window.location.href = 'assets/controladores/descargar_exam_final.php?codigo_a=' + encodeURIComponent(id_alumno);  
     });  
