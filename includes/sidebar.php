01a29cd3968eca99ebe3575aa0deea4205e95079
Miguel <miguel11.macj@gmail.com>
2024-08-21 03:05:54 -0500

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -2,6 +2,7 @@
     <a href="./../ver_perfil.php">MI PERFIL</a>
     <a href="./../tramites.php">TRÁMITES</a>
     <a href="./../constancia.php">CONSTANCIA</a>
+    <a href="./../descarga_constancia.php">DESCARGAR CONSTANCIA</a>
     <a href="./../detalles.php">NOTIFICACIONES</a>
     <a href="assets/controladores/cerrar_sesion.php" class="logout-btn tooltip">CERRAR SESION</a>
 </nav>
