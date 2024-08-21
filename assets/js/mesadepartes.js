41990a1b0d38c7ca7d42c8ea5e504f92be0e1c27
Miguel <miguel11.macj@gmail.com>
2024-08-21 00:05:20 -0500

    Update mesadepartes.js

--- assets/js/mesadepartes.js
+++ assets/js/mesadepartes.js
@@ -83,37 +83,6 @@ function closeModal() {
   document.getElementById('imageModal').style.display = 'none';
 }
 
-//VALIDACIONES PARA CARTA_PRESENTACION.PHP
-//para el ruc de la empresa
-document.getElementById("rucEmpresa").addEventListener("input",function(a){
-  let valor = a.target.value;
-  if(valor.length > 11){
-      a.target.value = valor.slice(0, 11);
-  }
-})
-//para el DNI del representante de la empresa
-document.getElementById("dniRepresentante").addEventListener("input",function(a){
-  let valor = a.target.value;
-  if(valor.length > 8){
-      a.target.value = valor.slice(0, 8);
-  }
-})
-//para el celular del representante de la empresa
-document.getElementById("celularRepresentante").addEventListener("input",function(a){
-  let valor = a.target.value;
-  if(valor.length > 9){
-      a.target.value = valor.slice(0, 9);
-  }
-})
-//para el número de liquidación del comprobante de pago
-document.getElementById("numLiquidacion").addEventListener("input",function(a){
-  let valor = a.target.value;
-  if(valor.length > 10){
-      a.target.value = valor.slice(0, 10);
-  }
-})
-
-
 
 //Botones de tramite
 function iniciar_cp() {
@@ -631,3 +600,13 @@ $(document).ready(function() {
       });  
   });  
 }); 
+
+$(document).ready(function() {  
+  $('#d_c_p').click(function() {  
+      // Obtener los valores de los inputs  
+      var id_alumno = $('#id_alumno').val();  
+      
+      // Redirigir a la URL de descarga  
+      window.location.href = 'assets/controladores/descargar_carta_pre.php?codigo_a=' + encodeURIComponent(id_alumno);  
+  });  
+});
